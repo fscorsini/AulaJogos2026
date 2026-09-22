ae5b5bf0c4271ac763f471deedcabbdbba243275
Fábio Corsini <fscorsini@gmail.com>
2026-09-22 08:31:27 -0300

    Aula dia 22/09/2026

--- inimigo.gd
+++ inimigo.gd
@@ -3,11 +3,18 @@ extends CharacterBody2D
 
 const SPEED = 700.0
 var direction = -1
+@onready var batevolta = $batevolta as RayCast2D
 
 func _physics_process(delta: float) -> void:
 	# Tratamento da gravidade
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+	if batevolta.is_colliding():
+		direction *= -1
+		batevolta.scale.x *= -1
+	
+	$animacao.scale.x = direction
 
 
 	# Movimento horizontal do personagem
