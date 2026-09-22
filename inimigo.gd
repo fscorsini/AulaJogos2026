06d8517446a18bc1243a3d3d7dd4a15e0c1156c6
Fábio Corsini <fscorsini@gmail.com>
2026-09-22 10:42:48 -0300

    Aula dia 22/09/2026

--- inimigo.gd
+++ inimigo.gd
@@ -10,11 +10,11 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+
 	if batevolta.is_colliding():
 		direction *= -1
 		batevolta.scale.x *= -1
-	
-	$animacao.scale.x = direction
+		$animacao.scale.x *= -1
 
 
 	# Movimento horizontal do personagem
